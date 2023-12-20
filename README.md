cb3ddcabcb61aabece5d04d381a6c6340ef969d0
RacoonYan <ran.yan@hyperisland.se>
2023-12-20 10:55:07 +0100

    add loader script

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # energi-loader
+
+## Setup
+
+Install requirements with: `pip install -r requirements.txt`
+
+## Running 
+
+The ETL script takes 4 arguments:
+| # | Desc                          | Example                    |
+|---|-------------------------------|----------------------------|
+| 1 | Path to sqllite db.           | data/energi.sqlite         |
+| 2 | First date to fetch data for. | 2023-01-01                 |
+| 3 | Last date to fetch date for.  | 2023-01-31                 |
+| 4 | List of energi DBs.           | CO2Emis,DeclarationGridmix |
+
+Each database will be loaded into a separete table with the database name as table name.
+
+Example command:
+
+```sh
+python src/main/python/energi_loader.py data/energi.sqlite 2023-01-01 2023-01-31 CO2Emis,DeclarationGridmix 
+```
--- src/main/python/energi_loader.py
+++ src/main/python/energi_loader.py
@@ -1,31 +1,105 @@
-import requests
+import argparse
 import datetime as dt
-import pandas as pd
 import os
+import sqlite3
 
-INTERESTING_DATASETS = ["CO2Emis", "DeclarationProduction", "DeclarationGridmix", "CapacityPerMunicipality"]
+import pandas as pd
+import requests
 
-def getDataset(dataset, day, limit=10):
-    end = (dt.datetime.strptime(day, "%Y-%m-%d") + dt.timedelta(days=1)).strftime("%Y-%m-%d")
+INTERESTING_DATASETS = [
+    "CO2Emis",
+    "DeclarationProduction",
+    "DeclarationGridmix",
+    "CapacityPerMunicipality",
+]
+KNOWN_DATETIME_FIELDS = ["Minutes5UTC", "Minutes5DK", "HourUTC", "HourDK"]
+
+
+def fetch_dataset_from_api(dataset, day, limit=10):
+    start = day.strftime("%Y-%m-%d")
+    end = (day + dt.timedelta(days=1)).strftime("%Y-%m-%d")
     try:
-        response = requests.get(f"https://api.energidataservice.dk/dataset/{dataset}?start={day}T00:00&end={end}&limit={limit}")
+        response = requests.get(
+            f"https://api.energidataservice.dk/dataset/{dataset}?"
+            + f"start={start}T00:00&end={end}&limit={limit}"
+        )
         return response.json()
-    except:
-        raise e
+    except Exception:
+        print("Exception during loading")
+        return {}
 
-def toDataframe(json_dict):
-    if json_dict.get('records'):
-        return pd.DataFrame.from_dict(json_dict['records'])
-    
-def getAsDataFrame(dataset, day, limit=10):
-    return toDataframe(getDataset(dataset, day, limit))
 
-def getAndSaveAsCsv(path, dataset, day, limit=10):
-    file = os.path.join(path, f"{dataset}.csv") 
-    df = toDataframe(getDataset(dataset, day, limit))
+def to_data_frame(json_dict):
+    if json_dict.get("records"):
+        return pd.DataFrame.from_dict(json_dict["records"])
+
+
+def get_as_data_frame(dataset, day, limit=10):
+    return to_data_frame(fetch_dataset_from_api(dataset, day, limit))
+
+
+def field_to_datetime(df, field):
+    df[field] = pd.to_datetime(df[field])
+
+
+def convert_known_datetime_fields(df):
+    for c in df.columns:
+        if c in KNOWN_DATETIME_FIELDS:
+            field_to_datetime(df, c)
+
+
+def transform(df, day):
+    df["date"] = day.strftime("%Y-%m-%d")
+    convert_known_datetime_fields(df)
+    return df.reset_index().set_index("index")
+
+
+def get_and_save_as_csv(path, dataset, day, limit=10):
+    file = os.path.join(path, f"{dataset}.csv")
+    df = to_data_frame(fetch_dataset_from_api(dataset, day, limit))
     if df is not None:
-        os.makedirs(path, exist_ok=True)
         df.to_csv(file)
-# # temporary for data exploration
-for ds in INTERESTING_DATASETS:
-    getAndSaveAsCsv(path="energi", dataset=ds, day="2023-01-01", limit=1000)
+
+
+def delete_if_day_exits(conn, table, day):
+    cur = conn.cursor()
+    # check if table exists
+    cur.execute(
+        f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table}';"
+    )
+    if len(cur.fetchall()) > 0:
+        # we already have written data for this table
+        cur.execute(f"DELETE FROM {table} WHERE date = '{day.strftime('%Y-%m-%d')}';")
+
+
+def upsert_into_sqlite(sqlite_db, dataset, day, df):
+    with sqlite3.connect(sqlite_db) as con:
+        delete_if_day_exits(con, dataset, day)
+        df.to_sql(dataset, con, if_exists="append", index=False)
+
+
+def etl(dataset, day, sqlite_db, limit=10000):
+    df_raw = get_as_data_frame(dataset, day, limit)
+    df_enriched = transform(df_raw, day)
+    upsert_into_sqlite(sqlite_db, dataset, day, df_enriched)
+
+
+def do_work(sqlite_db, date_start, date_end, databases):
+    dates = pd.date_range(start=date_start, end=date_end)
+    for db in databases:
+        for day in dates:
+            etl(db, day, sqlite_db)
+        print(f"Database '{db}' has been loaded into {sqlite_db}.")
+
+
+parser = argparse.ArgumentParser(
+    prog="EnergiLoader", description="Fetches Dataset from the danish energi API"
+)
+
+parser.add_argument("sqlite_db")
+parser.add_argument("date_start")
+parser.add_argument("date_end")
+parser.add_argument("databases")
+
+args = parser.parse_args()
+do_work(args.sqlite_db, args.date_start, args.date_end, args.databases.split(","))
